1a198ceb7a1409ce440fda1c31d0c8c823ecf093
Luke Logan <llogan@highpoint.edu>
2019-06-05 16:52:55 -0400

    finished collatz triangle

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,12 +1,26 @@
-export const steps = ((number, i=0) => {
-    i++;
-
-    if(number % 2 === 0 ){
-
-        number = number /2;
-        console.log(i);
-        steps(number);
+export const steps = (start) => {
+    // Catch Errors
+    if(start <= 0 ){
+        throw 'Only positive numbers are allowed';
     }
-    return i;
+    // Repeat the function as long as a condition is true:
+    // I need a loop!
+    for(var i = 0; start > 1; i++){
 
-});
+        // Check if any remainder when dividing. If not,
+        // it is an odd number
+        if(start % 2 !== 0 ){
+            start = (start * 3) + 1;
+            // add to i because it is still in the loop
+            i++;
+            }
+
+        if(start % 2 == 0 ){
+            // No need for i++ because it will
+            //loop again, adding 1;
+            start = start / 2;
+            }
+        }
+        return i;
+
+}
